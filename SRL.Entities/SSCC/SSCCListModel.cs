8b5ba7ca83de915d667236ed179e3eb335f25c51
Wietse Pot <wietse.pot@gmail.com>
2018-07-17 10:39:22 +0200

    Implement feature 78, conforming to the agreed upon architecture

---
+++ SRL.Entities/SSCC/SSCCListModel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRL.Models.SSCC
+{
+    public class SSCCListModel
+    {
+        public DateTime? OrderDate { get; set; }
+        public string SSCC { get; set; }
+        public string ActorFrom { get; set; }
+        public string ActorTo { get; set; }
+        public string SsccStatus { get; set; }
+        public double ValidationDeadline { get; set; }
+        public bool SlaOK { get; set; }
+        public bool CountingOK { get; set; }
+        public DateTime? CIDate { get; set; }
+        public bool IsValidated { get; set; }
+        public string ValidationStatus { get; set; }
+    }
+}
