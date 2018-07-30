dc0a13870c011e0719fd51efcf53dbe11eee62d5
Robert Donner <robert.donner@sogeti.com>
2018-07-30 12:03:17 +0200

    fix (actor-sscc): fix Actor and SSCC after merge-request

--- SRL_Portal_API/Controllers/ActorController.cs
+++ SRL_Portal_API/Controllers/ActorController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using SRL_Portal_API.Models;
+using SRL.Data_Access.Entity;
 
 namespace SRL_Portal_API.Controllers
 {
