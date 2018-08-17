a056dd11dd17e972c9566944c88794fd0c6ccf6c
Madhuri Gayke <madhuri.gayke@capgemini.com>
2018-08-17 14:03:48 +0200

    Chore(RetailerChainList) Fetch retailer chain list for internal users

---
+++ SRL.DataAccess/Repository/RetailerChainRepository.cs
@@ -0,0 +1,23 @@
+﻿using SRL.Models.RetailerChain;
+using System.Collections.Generic;
+using System.Linq;
+using SRL.Data_Access.Adapter;
+
+namespace SRL.Data_Access.Repository
+{
+    /// <summary>
+    /// Retailer chain repository to fetch its list from database
+    /// </summary>
+    public class RetailerChainRepository
+    {
+        public List<RetailerChain> GetRetailerChains()
+        {
+            List<RetailerChain> retailerChains = new List<RetailerChain>();
+            using (var cntx = new SRL.Data_Access.Entity.BACKUP_SRL_20180613Entities())
+            {
+                retailerChains = cntx.API_LIST_RETAILERS().ToList().ToEntityRetailerChainList();
+            }
+            return retailerChains;
+        }
+    }
+}
---
+++ SRL_Portal_API/Controllers/RetailerChainController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using SRL.Data_Access.Repository;
+using SRL.Models.RetailerChain;
+
+namespace SRL_Portal_API.Controllers
+{
+    /// <summary>
+    /// Controller to fetch Retailer Chains list
+    /// </summary>
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class RetailerChainController : ApiController
+    {
+        [Route("RetailerChains")]
+        [HttpGet]
+        public List<RetailerChain> GetRetailerChains()
+        {
+                        
+            RetailerChainRepository retailerChainRepository = new RetailerChainRepository();
+            return retailerChainRepository.GetRetailerChains();
+        }
+    }
+}
