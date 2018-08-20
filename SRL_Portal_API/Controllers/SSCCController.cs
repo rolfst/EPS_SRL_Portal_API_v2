dd6c15436937d3341b0e23a693612837a6969927
Madhuri Gayke <madhuri.gayke@capgemini.com>
2018-08-20 15:06:40 +0200

    Chore(SSCC List)Get all available SSCC numbers for filter

--- SRL_Portal_API/Controllers/SSCCController.cs
+++ SRL_Portal_API/Controllers/SSCCController.cs
@@ -126,7 +126,7 @@ namespace SRL_Portal_API.Controllers
                 RetailerChainId = retailerChainId
             };
 
-            return _ssccListRepository.GetSSCCList(request).Select(x => x.SSCC).Distinct().ToList();
+            return _ssccListRepository.GetSSCCNumberList(request).Select(x => x.SSCC).Distinct().ToList();
         }
 
         [HttpPost]
