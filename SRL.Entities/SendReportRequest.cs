e36ce04b4ad5108e593f54726f95e312df5ea173
Robert Donner <robert.donner@sogeti.com>
2018-08-24 16:21:06 +0200

    chore (report): add mail functionality to send mail to user.

---
+++ SRL.Entities/SendReportRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SRL.Models
+{
+    public class SendReportRequest
+    {
+        public string MailAddress { get; set; }
+        public int OrderNumber { get; set; }
+    }
+
+    public class SendReportRequestList
+    {
+        public List<SendReportRequest> Requests { get; set; }
+    }
+}
---
+++ SRL_Portal_API/Controllers/ReportsController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Http;
+using SRL.Models;
+
+namespace SRL_Portal_API.Controllers
+{
+    public class ReportsController : BaseController
+    {
+        [HttpPost]
+        public void SendReportsToActors(SendReportRequestList sendReportRequest)
+        {
+            // Send report to every request
+            foreach (var request in sendReportRequest.Requests)
+            {
+                // Validate mailAddresses
+                var mailAddress = new MailAddress(request.MailAddress);
+
+                SendReportToActor(mailAddress, request.OrderNumber);
+            }
+        }
+
+        private static void SendReportToActor(MailAddress mailAddress, int requestOrderNumber)
+        {
+            // Retrieve order
+            var controller = new OrderDetailController();
+            var orderDetail = controller.Get(requestOrderNumber);
+
+            var mail = new MailMessage("noreply@srlEuropoolsystems.com", mailAddress.Address)
+            {
+                Subject = $"Report for order {orderDetail.OrderNumber}",
+                Body = orderDetail.ToString()
+            };
+
+            var client = new SmtpClient
+            {
+                Port = 25,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Host = ConfigurationManager.AppSettings["smtpHost"],
+                Credentials = new NetworkCredential(ConfigurationManager.AppSettings["smtpUsername"], ConfigurationManager.AppSettings["smtpPassword"]),
+                EnableSsl = true
+            };
+            client.Send(mail);
+        }
+    }
+}
