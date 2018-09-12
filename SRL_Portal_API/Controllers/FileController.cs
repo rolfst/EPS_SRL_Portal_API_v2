cfc0c997e8ca765ac2efb7a027343cf7db41f207
Robert Donner <robert.donner@sogeti.com>
2018-09-12 13:39:43 +0200

    chore (file): add authorization to endpoint.

--- SRL_Portal_API/Controllers/FileController.cs
+++ SRL_Portal_API/Controllers/FileController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Http;
+using SRL.Models.Constants;
+using SRL_Portal_API.Common;
 
 namespace SRL_Portal_API.Controllers
 {
@@ -6,6 +8,7 @@ namespace SRL_Portal_API.Controllers
     public class FileController : BaseController
     {
         [HttpPost]
+        [CustomAuthorizationFilter(new string[] { UserRoles.CustomerServiceAgent, UserRoles.Customer, UserRoles.SuperUser, UserRoles.UltraUser, UserRoles.WebPortalAdministrator })]
         public string DownloadFile([FromBody] string path)
         {
             log.Info(string.Format(LogMessages.RequestMethod, RequestContext.Principal.Identity.Name, "file\\download"));
