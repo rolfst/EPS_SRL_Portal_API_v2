1c0dcefb401d248c9571703af0272881284ed3f3
Robert Donner <robert.donner@sogeti.com>
2018-09-12 13:38:45 +0200

    feature (file): add file download endpoint.

---
+++ SRL_Portal_API/Controllers/FileController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace SRL_Portal_API.Controllers
+{
+    [RoutePrefix("api")]
+    public class FileController : BaseController
+    {
+        [HttpPost]
+        public string DownloadFile([FromBody] string path)
+        {
+            log.Info(string.Format(LogMessages.RequestMethod, RequestContext.Principal.Identity.Name, "file\\download"));
+            return path;
+        }
+    }
+}
